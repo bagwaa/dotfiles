ef0568a48f0c56c18a99095d182678e1a036ce62
Richard Bagshaw <rbagshaw@sans.org>
2023-03-09 13:48:42 +0000

    Create ale.lua

---
+++ nvim/lua/user/plugins/ale.lua
@@ -0,0 +1,19 @@
+vim.cmd([[
+  highlight clear ALEErrorSign
+  highlight clear ALEWarningSign
+  let g:ale_linters = { 'php': ['php', 'phpstan'] }
+  let g:ale_fixers = { 'php': ['php_cs_fixer', 'phpcbf'] }
+]])
+
+vim.g.ale_linters_explicit = 1
+vim.g.ale_open_list = 1
+vim.g.ale_set_loclist = 0
+vim.g.ale_list_window_size = 5
+vim.g.ale_fix_on_save = 1
+
+if vim.fn.filereadable("ruleset.xml") == 1 then
+    vim.g.ale_php_phpcbf_standard='./ruleset.xml'
+    vim.g.ale_php_phpcs_standard='./ruleset.xml'
+end
+
+
