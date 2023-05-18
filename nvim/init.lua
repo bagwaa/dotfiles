856dbf86027f91cf1fb2f6b056bfbb6a035f5b35
Richard Bagshaw <me@richardbagshaw.co.uk>
2023-05-18 16:40:56 +0100

    open telescope on entering neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
 require("user/options")
 require("user/plugins")
 require("user/keymaps")
+
+-- open telescope on starting neovim
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.cmd("Telescope find_files")
+    end,
+});
