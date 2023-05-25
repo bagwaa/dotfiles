ef73b770ec4ed498555321b90aa1530dff16f5ab
Richard Bagshaw <me@richardbagshaw.co.uk>
2023-05-25 16:12:13 +0100

    clean up

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -4,24 +4,19 @@ local lspkind = require("lspkind")
 
 require("luasnip/loaders/from_snipmate").lazy_load()
 
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup({
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body)
-	end,
+        end,
     },
     formatting = {
         format = lspkind.cmp_format({
-	    with_text = true,
-	    menu = {
+            with_text = true,
+            menu = {
                 nvim_lsp = "[LSP]",
-		nvim_lua = "[Lua]",
-		buffer = "[BUF]",
+                nvim_lua = "[Lua]",
+                buffer = "[BUF]",
             },
         }),
     },
@@ -34,7 +29,7 @@ cmp.setup({
             else
                 fallback()
             end
-         end,
+        end,
         ["<S-Tab>"] = function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
@@ -48,11 +43,11 @@ cmp.setup({
     },
     sources = {
         { name = "nvim_lsp" },
-	{ name = "nvim_lsp_signature_help" },
-	{ name = "nvim_lua" },
-	{ name = "luasnip" },
-	{ name = "path" },
-	{ name = "buffer" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer" },
         { name = "crates" },
     },
     experimental = {
--- nvim/lua/user/plugins/neo-tree.lua
+++ nvim/lua/user/plugins/neo-tree.lua
@@ -54,13 +54,7 @@ require("neo-tree").setup({
 	}
 })
 
-vim.fn.sign_define("DiagnosticSignError",
-	{ text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn",
-	{ text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo",
-	{ text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint",
-	{ text = "", texthl = "DiagnosticSignHint" })
--- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#1e1e2e" })
--- vim.api.nvim_set_hl(0, "NvimTreeIndentMarker", { fg = "#404040" })
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
