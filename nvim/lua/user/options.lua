194b4f97b3dcf470db4a5b3dca6057984f640f4b
Richard Bagshaw <richard.bagshaw@mumsnet.com>
2024-08-12 08:23:47 +0100

    Code folding using za

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -8,6 +8,26 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 
+-- folding
+vim.opt.foldcolumn = "0"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+
+vim.opt.foldnestmax = 3
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
+local function close_all_folds()
+    vim.api.nvim_exec2("%foldc!", { output = false })
+end
+local function open_all_folds()
+    vim.api.nvim_exec2("%foldo!", { output = false })
+end
+
+vim.keymap.set("n", "<leader>zs", close_all_folds, { desc = "[s]hut all folds" })
+vim.keymap.set("n", "<leader>zo", open_all_folds, { desc = "[o]pen all folds" })
+
 -- wrapping
 vim.opt.wrap = false
 
