cdd22fcc1923418f4bed5e8289756d4e159586f1
Richard Bagshaw <richard@bagshaw.co.uk>
2025-02-14 14:00:26 +0000

    Set the default test runner to Pest

--- nvim/lua/user/plugins/vim-test.lua
+++ nvim/lua/user/plugins/vim-test.lua
@@ -8,11 +8,11 @@ vim.cmd([[
   let g:test#strategy = "basic"
   let g:shtuff_receiver = 'test-runner'
 
-  let g:test#php#runner = 'phpunit'
-  let g:test#php#phpunit#executable = 'php artisan test --parallel --stop-on-failure'
-  let test#php#phpunit#options = '--colors=always'
+  let g:test#php#runner = 'pest'
+  let g:test#php#phpunit#executable = './vendor/bin/pest'
+  " let test#php#phpunit#options = '--colors=always'
   let g:test#echo_command = 0
 
-  let g:test#go#gotest#executable = 'richgo test'
+  let g:test#go#gotest#executable = 'richgo test -v'
 ]])
 
