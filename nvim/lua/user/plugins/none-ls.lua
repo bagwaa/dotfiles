af048d605ab07c1d44c4d30dd0a0e3f2d3f244cc
Richard Bagshaw <richard@bagshaw.co.uk>
2025-02-14 14:01:12 +0000

    Add none-ls and telescope-ui-select config

---
+++ nvim/lua/user/plugins/none-ls.lua
@@ -0,0 +1,11 @@
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.pint,
+        null_ls.builtins.diagnostics.phpstan,
+    },
+})
