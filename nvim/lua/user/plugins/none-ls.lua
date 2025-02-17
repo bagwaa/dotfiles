4742df0e477cb80e83569df7f8f74baf59da0eac
Richard Bagshaw <richard@bagshaw.co.uk>
2025-02-17 11:08:07 +0000

    Update none-ls config to only load under certain conditions and not in tests for PHP

--- nvim/lua/user/plugins/none-ls.lua
+++ nvim/lua/user/plugins/none-ls.lua
@@ -1,12 +1,44 @@
 local null_ls = require("null-ls")
 
+local function in_tests_dir()
+    local path = vim.api.nvim_buf_get_name(0)
+    return string.match(path, "tests/")
+end
+
 null_ls.setup({
     sources = {
+        -- apply globally
         null_ls.builtins.completion.spell,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.pint,
-        null_ls.builtins.diagnostics.phpstan,
-        null_ls.builtins.formatting.blade_formatter,
+
+        -- apply only on lua files
+        null_ls.builtins.formatting.stylua.with({
+            filetypes = { "lua" },
+        }),
+
+        -- apply only on blade files
+        null_ls.builtins.formatting.blade_formatter.with({
+            filetypes = { "blade" },
+        }),
+
+        -- apply only on javascript files
+        null_ls.builtins.formatting.prettierd.with({
+            filetypes = { "javascript", "typescript", "json", "vue", "yaml", "html" },
+        }),
+
+        -- apply only on php files outside of tests directory
+        null_ls.builtins.diagnostics.phpstan.with({
+            filetypes = { "php" },
+            condition = function()
+                return not in_tests_dir()
+            end,
+        }),
+
+        -- apply only on php files outside of tests directory
+        null_ls.builtins.formatting.pint.with({
+            filetypes = { "php" },
+            condition = function()
+                return not in_tests_dir()
+            end,
+        }),
     },
 })
